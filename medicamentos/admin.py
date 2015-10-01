f8ba4c55dc2be1b3b3f5fef1844606a708636925
adriangw <adrianwilliamsg2684@gmail.com>
2015-10-01 15:38:23 -0300

    modelo medicamento

--- medicamentos/admin.py
+++ medicamentos/admin.py
@@ -1,8 +1,17 @@
 from django.contrib import admin
 from .models import Monodroga
 from .models import Dosis
+from .models import Medicamento
+from .models import Formula
+from .models import Presentacion
+from .models import NombreFantasia
+from .models import Formula
+
 
 # Register your models here.
-
+admin.site.register(Medicamento)
 admin.site.register(Monodroga)
 admin.site.register(Dosis)
+admin.site.register(Formula)
+admin.site.register(Presentacion)
+admin.site.register(NombreFantasia)
--- medicamentos/models.py
+++ medicamentos/models.py
@@ -2,21 +2,47 @@ from django.db import models
 
 # Create your models here.
 
+class Medicamento(models.Model):
+	formulas = models.ManyToManyField('Formula')
+	nombreFantasia = models.ForeignKey('NombreFantasia')
+	presentacion = models.ManyToManyField('Presentacion')
+	codigoBarras = models.CharField(max_length=15)
+	stockMinimo = models.IntegerField()
+	precio = models.FloatField()
+	
+	def __str__(self):
+		return self.codigoBarras
+
+class NombreFantasia(models.Model):
+	nombreF = models.CharField(max_length=30)		
+	
+	def __str__(self):
+		return self.nombreF
+	
+class Presentacion(models.Model):
+	descripcion = models.TextField()
+	cantidad = models.IntegerField()
+	unidadMedida = models.CharField(max_length=50)
+	
+	def __str__(self):
+		return self.descripcion
+		
+class Formula(models.Model):
+	monodroga = models.ForeignKey('Monodroga',on_delete = models.CASCADE)		
+	dosis = models.ForeignKey('Dosis',on_delete = models.CASCADE)
+
+	
 class Monodroga(models.Model):
-    FILTROS = ["nombre__icontains"]
+    #FILTROS = ["nombre__icontains"]
     nombre = models.CharField(max_length=100)
 
     def __str__(self):
         return self.nombre
 
-
 class Dosis(models.Model):
-    monodroga = models.ForeignKey('Monodroga')
-    unidadMedida = models.CharField(max_length=100)
+    unidadMedida = models.CharField(max_length=4)
     cantidad = models.IntegerField()
 
-    def __str__(self):
-        return self.unidadMedida
 
 
 
