3403648f1a4daee52dc1dbdce4041391def24a9f
mzkzr <mzkzrrw@gmail.com>
2015-10-08 17:38:30 -0300

    baja organizaciones v2

--- organizaciones/views.py
+++ organizaciones/views.py
@@ -23,7 +23,7 @@ def farmacias(request):
          "filtros": filters})
 
 @login_required(login_url='login')
-def farmacias_add(request):
+def farmacia_add(request):
     if request.method == "POST":
         form = forms.FarmaciaForm(request.POST)
         if form.is_valid():
@@ -36,15 +36,22 @@ def farmacias_add(request):
         {"form": form})
 
 @login_required(login_url='login')
-def farmacias_delete(request, id):
+def farmacia_delete(request, id):
     farmacia = models.Farmacia.objects.get(pk = id)
     farmacia.delete()
     return redirect('farmacias')
 
 @login_required(login_url='login')
 def clinicas(request):
-    clinicas = None
-    filters = None
+    filters = get_filtros(request.GET, models.Clinica)
+    mfilters = dict(filter(lambda v: v[0] in models.Clinica.FILTROS, filters.items()))
+    clinicas = models.Clinica.objects.filter(**mfilters)
+    return render(request, "clinicas.html",
+         {"clinicas": clinicas,
+         "filtros": filters})
+
+@login_required(login_url='login')
+def clinica_add(request):
     if request.method == "POST":
         form = forms.ClinicaForm(request.POST)
         if form.is_valid():
@@ -53,34 +60,39 @@ def clinicas(request):
             return redirect('clinicas')
     else:
         form = forms.ClinicaForm()
-        filters = get_filtros(request.GET, models.Clinica)
-        mfilters = dict(filter(lambda v: v[0] in models.Clinica.FILTROS, filters.items()))
-        clinicas = models.Clinica.objects.filter(**mfilters)
     return render(request, "clinicas.html",
-        {"clinicas": clinicas,
-         "filtros": filters,
-         "form": form})
+        {"form": form})
+
+@login_required(login_url='login')
+def clinica_delete(request, id):
+    clinica = models.Clinica.objects.get(pk = id)
+    clinica.delete()
+    return redirect('clinicas')
 
 @login_required(login_url='login')
 def laboratorios(request):
-    laboratorios = None
-    filters = None
+    filters = get_filtros(request.GET, models.Laboratorio)
+    mfilters = dict(filter(lambda v: v[0] in models.Laboratorio.FILTROS, filters.items()))
+    laboratorios = models.Laboratorio.objects.filter(**mfilters)
+    return render(request, "laboratorios.html",
+         {"laboratorios": laboratorios,
+         "filtros": filters})
+
+@login_required(login_url='login')
+def laboratorio_add(request):
     if request.method == "POST":
-
         form = forms.LaboratorioForm(request.POST)
-
         if form.is_valid():
             print(form.cleaned_data)
             form.save()
             return redirect('laboratorios')
-        else:
-            print("holass")
     else:
         form = forms.LaboratorioForm()
-        filters = get_filtros(request.GET, models.Laboratorio)
-        mfilters = dict(filter(lambda v: v[0] in models.Laboratorio.FILTROS, filters.items()))
-        laboratorios = models.Laboratorio.objects.filter(**mfilters)
     return render(request, "laboratorios.html",
-        {"laboratorios": laboratorios,
-         "filtros": filters,
-         "form": form})
+        {"form": form})
+
+@login_required(login_url='login')
+def laboratorio_delete(request, id):
+    laboratorio = models.Laboratorio.objects.get(pk = id)
+    laboratorio.delete()
+    return redirect('laboratorios')
