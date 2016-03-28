f03b2959986362bd306e125d371fdd3d323032a5
mzkzr <mzkzrrw@gmail.com>
2016-03-28 17:50:30 -0300

    herencia remito

--- farma/urls.py
+++ farma/urls.py
@@ -102,7 +102,7 @@ urlpatterns = [
     url(r'^pedidosDeFarmacia/add/detalles/add/$', pviews.detallePedidoDeFarmacia_add, name="detallePedidoDeFarmacia_add"),
     url(r'^pedidosDeFarmacia/add/detalles/update/(?P<id_detalle>\d+)/$', pviews.detallePedidoDeFarmacia_update, name="detallePedidoDeFarmacia_update"),
     url(r'^pedidosDeFarmacia/add/detalles/delete/(?P<id_detalle>\d+)/$', pviews.detallePedidoDeFarmacia_delete, name="detallePedidoDeFarmacia_delete"),
-    url(r'^pedidosDeFarmacia/add/detalles/remitoPDF/(?P<id_pedido>\d+)/$', pviews.remitoPDF.as_view(), name="remitoPDF"),
+    url(r'^pedidosDeFarmacia/remitoFarmacia/(?P<id_pedido>\d+)/$', pviews.remitoFarmacia.as_view(), name="remitoFarmacia"),
 
 
     # ************************************** Pedido de Clinica ***************************************
